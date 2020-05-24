e2c86de942062dd1996a2c17b850ee5403df8410
Matt Gibson <gothick@gothick.org.uk>
2020-05-24 12:31:12 +0100

    Retrieving playlists okay.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import creds
+import spotipy.util as util
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+username  = 'gothick'
 
-results = sp.search(q='Honeyblood', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+scope = 'streaming user-modify-playback-state playlist-read-private app-remote-control'
+
+token = util.prompt_for_user_token(
+    username,
+    scope,
+    client_id = creds.SPOTIPY_CLIENT_ID,
+    client_secret = creds.SPOTIPY_CLIENT_SECRET,
+    redirect_uri = creds.REDIRECT_URI
+    )
+
+if token:
+
+    sp = spotipy.Spotify(auth=token)
+
+    results = sp.search(q='Honeyblood', limit=20)
+    for idx, track in enumerate(results['tracks']['items']):
+        print(idx, track['name'])
+
+    playlists = sp.current_user_playlists(limit = 50, offset = 0)
+    for playlist in playlists['items']:
+        print(playlist['name'])
+else:
+    print('Could not authenticate.')
