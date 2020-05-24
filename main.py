a2dad9fccba375ade42678d928a530b8e5c0bb56
Matt Gibson <gothick@gothick.org.uk>
2020-05-24 13:34:22 +0100

    Now getting back the right playlist.

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import creds
 import spotipy.util as util
 
 username  = 'gothick'
-
 scope = 'streaming user-modify-playback-state playlist-read-private app-remote-control'
+
+def get_playlists(sp):
+    results = sp.current_user_playlists(limit = 50, offset =0)
+    playlists = results['items']
+    while results['next']:
+        results = sp.next(results)
+        playlists.extend(results['items'])
+    return playlists
+
+def find_playlist_by_name(sp, name):
+    results = sp.current_user_playlists(limit = 50, offset = 0)
+    playlists = results['items']
+    playlist = next(filter(lambda p: p['name'] == name, playlists), None)
+    while playlist is None and results['next']:
+        results = sp.next(results)
+        playlists = results['items']
+        playlist = next(filter(lambda p: p['name'] == name, playlists), None)
+    return playlist
 
 token = util.prompt_for_user_token(
     username,
@@ -12,18 +29,11 @@ token = util.prompt_for_user_token(
     client_id = creds.SPOTIPY_CLIENT_ID,
     client_secret = creds.SPOTIPY_CLIENT_SECRET,
     redirect_uri = creds.REDIRECT_URI
-    )
+)
 
 if token:
-
     sp = spotipy.Spotify(auth=token)
+    playlist = find_playlist_by_name(sp, "Future Listening")
 
-    results = sp.search(q='Honeyblood', limit=20)
-    for idx, track in enumerate(results['tracks']['items']):
-        print(idx, track['name'])
-
-    playlists = sp.current_user_playlists(limit = 50, offset = 0)
-    for playlist in playlists['items']:
-        print(playlist['name'])
 else:
     print('Could not authenticate.')
