b907b295bf96350635c92ba062c988c699bc3ce0
Matt Gibson <gothick@gothick.org.uk>
2020-05-31 15:54:32 +0100

    Add example creds file

---
+++ creds_example.py
@@ -0,0 +1,7 @@
+# Copy this as creds.py and fill in the details
+SPOTIPY_CLIENT_ID="this is your spotify app's client ID"
+SPOTIPY_CLIENT_SECRET="and its secret"
+# This must match the redirect URI you've set in your app. Spotipy automatically
+# handles firing up a web browser for you during the OAuth flow if you've provided
+# a localhost address, which is handy.
+REDIRECT_URI="http://localhost:9090/" 
