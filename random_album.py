e4c39177ec44e02818bf983101e5ae1145e8230e
Matt Gibson <gothick@gothick.org.uk>
2023-01-02 19:25:20 +0000

    Apparenlty we need to renew the token and recreate the Spotipy object every time we use it. Code's still in better shape now, though.

--- random_album.py
+++ random_album.py
@@ -5,29 +5,36 @@ import logging
 
 class RandomAlbum:
     def __init__(self, username, client_id, client_secret, redirect_uri):
-        self.scope = scope = 'streaming user-read-playback-state user-modify-playback-state playlist-read-private app-remote-control'
+        self.scope = 'streaming user-read-playback-state user-modify-playback-state playlist-read-private app-remote-control'
+        self.username = username
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+
+    def __get_sp(self):
         logging.info('Prompting for user token.')
-        token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+        token = util.prompt_for_user_token(self.username, self.scope, self.client_id, self.client_secret, self.redirect_uri)
         if token:
-            self.sp = spotipy.Spotify(auth=token)
+            sp = spotipy.Spotify(auth=token)
         else:
             # Getting the token will likely throw an exception rather than
             # just return Nothing, but just in case...
             print('Could not authenticate.')
-            self.sp = None
+            sp = None
+        return sp
 
-    def find_playlist_by_name(self, name):
-        results = self.sp.current_user_playlists(limit = 50, offset = 0)
+    def __find_playlist_by_name(self, sp, name):
+        results = sp.current_user_playlists(limit = 50, offset = 0)
         playlists = results['items']
         playlist = next(filter(lambda p: p['name'] == name, playlists), None)
         while playlist is None and results['next']:
-            results = self.sp.next(results)
+            results = sp.next(results)
             playlists = results['items']
             playlist = next(filter(lambda p: p['name'] == name, playlists), None)
         return playlist
 
-    def find_device_id_by_name(self, name):
-        results = self.sp.devices()
+    def __find_device_id_by_name(self, sp, name):
+        results = sp.devices()
         devices = results['devices']
         device = next(filter(lambda d: d['name'] == name, devices), None)
         if device:
@@ -35,10 +42,11 @@ class RandomAlbum:
 
     def toggle_playback(self) -> bool:
         did_something = False
-        p = self.sp.current_playback()
+        sp = self.__get_sp()
+        p = sp.current_playback()
         if p and p['is_playing']:
             logging.info('Pausing playback')
-            self.sp.pause_playback()
+            sp.pause_playback()
             did_something = True
         else:
             # Wasn't playing. But we can't just start;
@@ -46,7 +54,7 @@ class RandomAlbum:
             logging.debug(p)
             if p:
                 logging.info('Resuming playback')
-                self.sp.start_playback()
+                sp.start_playback()
                 did_something = True
             else:
                 logging.info('Nothing to resume')
@@ -54,13 +62,14 @@ class RandomAlbum:
 
     def play_random_album(self, target_playlist, device_name, album_minimum_tracks = 0):
         # Find our Future Listening playlist.
-        playlist = self.find_playlist_by_name(target_playlist)
+        sp = self.__get_sp()
+        playlist = self.__find_playlist_by_name(sp, target_playlist)
         total_tracks = playlist['tracks']['total']
 
         while True:
             # Pick a random song from it
             random_track = random.randint(0, total_tracks - 1)
-            tracks = self.sp.playlist_tracks(playlist['id'],limit=1, offset=random_track)
+            tracks = sp.playlist_tracks(playlist['id'],limit=1, offset=random_track)
             track = tracks['items'][0]['track']
             print(f"Found track {track['name']} by {track['artists'][0]['name']}")
             print(f"...from an album called {track['album']['name']}")
@@ -73,7 +82,7 @@ class RandomAlbum:
         # Found an album with a decent number of tracks. Play it!
         device_id = None
         if device_name is not None:
-            device_id = self.find_device_id_by_name(device_name)
+            device_id = self.__find_device_id_by_name(sp, device_name)
         album_uri = track['album']['uri']
-        self.sp.start_playback(context_uri = album_uri, device_id = device_id)
+        sp.start_playback(context_uri = album_uri, device_id = device_id)
         print("Started playback of entire album.")
